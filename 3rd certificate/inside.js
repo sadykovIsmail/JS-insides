a527aaf4950ed0703aa366752daf8854f4a64834
Ismail <ghvh8176@gmail.com>
2025-04-30 20:37:32 +0600

    3rd certificate

---
+++ 3rd certificate/inside.js
@@ -0,0 +1,30 @@
+/*1) an argument.test(from) testing
+  2) .some() returns true when finds one true otherwise false
+const arr = ["A", "b", "C"];
+arr.some(letter => letter === letter.toUpperCase())
+ 3) .reduce is making one valuearray.reduce((accumulator, currentValue) => {
+// do something
+return accumulator;
+}, initialValue);
+ 4)getMean(numbers) gets average of numbers;
+ 5) Object.values(obj)	Array of values
+ 6) new Set(array)	Removes duplicate values
+ 7)Object.keys(obj)	Array of keys (names)
+ 8) Math.min() gets minimum max gets max 
+ 9) const base = 4;
+const exponent = 0.5;
+// returns 2
+Math.pow(base, exponent) it is base/expp.
+ 10) Math.sqrt() is 1/2
+ 11) .charCodeAt(0) returns unicode of strings
+ 12) Math.ceil (1.5)=> 2 is rounds up number 
+ 13) .onchange works when the vakue changes
+ 14) .target,
+it tells you which element the user changed, clicked, hovered, etc.
+ 15) .slice() is copyeing array taking slice
+ 16) .map(addCharacters(char1)(char2) is passing char1 to first and 2 which is inside of char1 
+  17) parseFloat is makes float
+ 18) hasOwnProperty() returns if there a property in object returns true or false
+19) Array.from() makes that thing array
+ 21) const arr = ["A", "b", "C"];
+arr.every(letter => letter === letter.toUpperCase());  */
