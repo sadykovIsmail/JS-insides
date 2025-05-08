a36a33329162d7d9402a03f6875a987b71426c29
Ismail <ghvh8176@gmail.com>
2025-05-09 05:27:47 +0600

    libraries

---
+++ Full-stack/front-end/React/inside.js
@@ -0,0 +1,6 @@
+/*1) Libraries (e.g., jQuery) offer focused tools for specific tasks like DOM manipulation and are flexible, giving developers more control.
+
+2) Frameworks (e.g., Angular, Remix, Next.js) provide structured architecture and enforce rules for building entire applications.
+
+3) React is a hybrid example—a UI library that's often used like a framework due to its flexibility and ecosystem.
+*/
