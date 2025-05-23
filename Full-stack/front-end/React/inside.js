ac852459b8b56eb74232db3df1b95661e134616e
Ismail <ghvh8176@gmail.com>
2025-05-23 07:04:54 +0600

    not to use effect

--- Full-stack/front-end/React/inside.js
+++ Full-stack/front-end/React/inside.js
@@ -273,4 +273,12 @@ Prevent unintended side-effects or extra runs
 Linting
 Trust react-hooks/exhaustive-deps to catch missing dependencies
 Don’t suppress it—refactor or move values so that dependencies are correct
-*/
+
+32) When not use useEfffects If you can calculate something during render, you don’t need an Effect.
+To cache expensive calculations, add useMemo instead of useEffect.
+To reset the state of an entire component tree, pass a different key to it.
+To reset a particular bit of state in response to a prop change, set it during rendering.
+Code that runs because a component was displayed should be in Effects, the rest should be in events.
+If you need to update the state of several components, it’s better to do it during a single event.
+Whenever you try to synchronize state variables in different components, consider lifting state up.
+You can fetch data with Effects, but you need to implement cleanup to avoid race conditions.  */
