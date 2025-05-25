57bf6b68c4cf61bf5d52290fc20d9d27c862ef02
Ismail <ghvh8176@gmail.com>
2025-05-25 23:33:54 +0600

    a

--- Full-stack/front-end/React/inside.js
+++ Full-stack/front-end/React/inside.js
@@ -2,6 +2,7 @@
 1) Libraries (e.g., jQuery) offer focused tools for specific tasks like DOM manipulation and are flexible, giving developers more control.
 
 2)Frameworks (e.g., Angular, Remix, Next.js) provide structured architecture and enforce rules for building entire applications.
+
 3) React is a hybrid example—a UI library that's often used like a framework due to its flexibility and ecosystem.
 4) SPAs provide a modern, app-like experience, but developers must address accessibility, navigation, SEO, and performance to ensure quality.
 5) React is a component-based JavaScript library for building fast, dynamic web UIs. It's popular due to its reusability, efficient updates, and developer-friendly features like state handling and the virtual DOM
