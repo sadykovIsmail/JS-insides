c192f91afbfdaefac5b511d3dc3905e0194c1da8
Ismail <ghvh8176@gmail.com>
2025-06-13 12:53:02 -0500

    r

--- Full-stack/front-end/React/inside.js
+++ Full-stack/front-end/React/inside.js
@@ -201,7 +201,7 @@ function MergedButton({ primary }) {
     ? { backgroundColor: "blue", color: "white" }
     : { backgroundColor: "gray", color: "black" };
 
-  rturn <button style={{ ...baseStyle, ...primaryStyle }}>Click</button>;
+  return <button style={{ ...baseStyle, ...primaryStyle }}>Click</button>;
 }
 Order matters: later properties override earlier ones.
 19) One root: wrap siblings in <></> is fragment or a container.   camelCase props: className, htmlFor, strokeWidth, etc.
