6171e5ab7ada901f687ad0cd7127ac13a31da492
Ismail <ghvh8176@gmail.com>
2025-06-14 12:53:12 -0500

    f

--- Full-stack/front-end/React/inside.js
+++ Full-stack/front-end/React/inside.js
@@ -396,7 +396,7 @@ RenderName.propTypes = {
   name: PropTypes.string.isRequired,
 }
   
-41) useParams() lets you access dynamic values from the URL like /profile/:name
+41) seParams() lets you access dynamic values from the URL like /profile/:name
 
 42) npm install react-router-dom to install router
 
