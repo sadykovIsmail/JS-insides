43dc36f8992af0e740c3340eb92ab4e534b602a9
Ismail <ghvh8176@gmail.com>
2025-06-14 12:54:12 -0500

    b

--- Full-stack/front-end/React/inside.js
+++ Full-stack/front-end/React/inside.js
@@ -394,7 +394,7 @@ toMatchSnapshot()	Useful for verifying layout, risky when overused
 40) propTypes is giving the exact propTypes in returnig not in testing 
 RenderName.propTypes = {
   name: PropTypes.string.isRequired,
-}bbbb
+}bbbbb
   
 41) useParams() lets you access dynamic values from the URL like /profile/:name
 
