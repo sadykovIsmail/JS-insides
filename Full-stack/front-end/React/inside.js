dfeb04594d3c8ef51f2f7f819df8e1f9ca8b871d
Ismail <ghvh8176@gmail.com>
2025-06-16 09:59:32 -0500

    useReduceer

--- Full-stack/front-end/React/inside.js
+++ Full-stack/front-end/React/inside.js
@@ -409,4 +409,12 @@ export const ShopContext = createContext({
   addToCart: () => {},
 });
   use provider:     <ShopContext.Provider value={{ cartItems, addToCart }}>
- */
+ 
+45)To convert from useState to useReducer:
+Dispatch actions from event handlers.
+Write a reducer function that returns the next state for a given state and action.
+Replace useState with useReducer.
+Reducers require you to write a bit more code, but they help with debugging and testing.
+Reducers must be pure.
+Each action describes a single user interaction.
+Use Immer if you want to write reducers in a mutating style. */
