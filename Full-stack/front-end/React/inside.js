920f88acfe350adf654bb0795a9ccb27dd319310
Ismail <ghvh8176@gmail.com>
2025-06-19 13:52:16 -0500

    d

--- Full-stack/front-end/React/inside.js
+++ Full-stack/front-end/React/inside.js
@@ -415,7 +415,7 @@ Dispatch actions from event handlers.
 Write a reducer function that returns the next state for a given state and action.
 Replace useState with useReducer.
 Reducers require you to write a bit more code, but they help with debugging and testing.
-Reucers must be pure.
+Reducers must be pure.
 Each action describes a single user interaction.
 Use Immer if you want to write reducers in a mutating style. 
 
