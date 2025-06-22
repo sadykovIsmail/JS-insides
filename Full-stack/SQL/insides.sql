5d97986915c138b57508997f09942599ea24b023
Ismail <ghvh8176@gmail.com>
2025-06-22 18:17:11 -0500

    wherte

--- Full-stack/SQL/insides.sql
+++ Full-stack/SQL/insides.sql
@@ -149,3 +149,10 @@
 -- ON character.id = character_tv_show.character_id
 -- INNER JOIN tv_show
 -- ON character_tv_show.tv_show_id = tv_show.id;
+
+-- 22) inner join using with WHERE SELECT character.name, tv_show.name
+-- FROM character 
+-- INNER JOIN character_tv_show
+-- ON character.id = character_tv_show.character_id
+-- INNER JOIN tv_show
+-- ON character_tv_show.tv_show_id = tv_show.id WHERE character.name != 'Willow Rosenberg' AND tv_show.name != 'How I Met Your Mother'
