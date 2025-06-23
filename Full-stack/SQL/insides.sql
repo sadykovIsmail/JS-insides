f7659e5ad238319a17c1cf8ce99479f484aeea73
Ismail <ghvh8176@gmail.com>
2025-06-22 20:05:41 -0500

    case

--- Full-stack/SQL/insides.sql
+++ Full-stack/SQL/insides.sql
@@ -183,3 +183,9 @@
 
 -- 27) _ matches one element % any
 -- SELECT * FROM robots WHERE name LIKE "%Robot 20__" 
+
+28) SELECT *,
+CASE WHEN species = 'human' THEN 'talk'
+WHEN species = 'dog' Then 'bark'
+ELSE 'meow' END AS sound
+FROM friends_of_pickles;
