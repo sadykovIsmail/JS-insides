a417518fc2eee2a5a764456f8a918913e83d6a9f
Ismail <ghvh8176@gmail.com>
2025-06-26 13:30:15 -0500

    s

--- Full-stack/SQL/insides.sql
+++ Full-stack/SQL/insides.sql
@@ -205,4 +205,4 @@
 --   SELECT population FROM world y
 --   WHERE y.continent = x.continent
 -- );
--- sselects the most population in each continentxs
+-- selects the most population in each continentxs
