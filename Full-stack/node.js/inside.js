78d844298bb19d462890d6bd4cf9a88f6a067815
Ismail <ghvh8176@gmail.com>
2025-07-01 08:50:12 -0500

    HTTPS

--- Full-stack/node.js/inside.js
+++ Full-stack/node.js/inside.js
@@ -1,6 +1,87 @@
 /* 1) You want the script to auto-restart when you make changes during development (node --watch filename.js)
-
 You want to run it with a shebang as a system script (./filename.js, after chmod +x filename.js and adding #!/usr/bin/env node at the top)
-
 You want to run scripts defined in your package.json via node --run <script>, npm run <script>, or yarn run <script> 
-*/ 
+
+ 2) GET with AXIOUS framework
+const axios = require('axios');
+
+axios
+  .get('https://example.com/todos')
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log(res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+  3) POST with AXIOUS
+axios
+  .post('https://whatever.com/todos', {
+    todo: 'Buy the milk',
+  })
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log(res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+  4) GET MANUALLY with https
+  const https = require('https');
+
+const options = {
+  hostname: 'example.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET',
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('data', d => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+
+req.end();
+5) POST WITH HTTPS MANUALLY
+const https = require('https');
+
+const data = JSON.stringify({
+  todo: 'Buy the milk',
+});
+
+const options = {
+  hostname: 'whatever.com',
+  port: 443,
+  path: '/todos',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length,
+  },
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('data', d => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+
+req.write(data);   // send the JSON data in the request
+req.end();         // finish the request
+
+  */ 
