c9b7e4de9639c41b628a890c3c490453216a1ed9
Ismail <ghvh8176@gmail.com>
2025-07-01 08:20:38 -0500

    k

---
+++ Full-stack/node.js/inside.js
@@ -0,0 +1,6 @@
+/* 1) You want the script to auto-restart when you make changes during development (node --watch filename.js)
+
+You want to run it with a shebang as a system script (./filename.js, after chmod +x filename.js and adding #!/usr/bin/env node at the top)
+
+You want to run scripts defined in your package.json via node --run <script>, npm run <script>, or yarn run <script> 
+*/ 
