f6c2767d56d6c85b5e5b96e9df523dccddb413f8
Ismail <ghvh8176@gmail.com>
2025-11-04 12:30:51 -0600

    data types

--- Full-stack Django/insight.py
+++ Full-stack Django/insight.py
@@ -12,4 +12,8 @@ print('Processing...', end=' ', flush=True)
 time.sleep(2)
 print('Done!')
 
+3) data types: integer: 4, -5 , float:4.50 , complex: 3 + 4j , set:{4, 2, 0}, dictionary: {'name': 'Ismail'} , typle: (4,2,0) , range(5) #(0, 5) , 
+list = [22, 'Hello', 3.24, true] , None # is nothing
+
+4) immutable data types: string, integer, float, boolean, tuple, and range. #can reassing but not change the value
 '''
