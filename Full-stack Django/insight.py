783a40801be1a74e000c5518fd0403d2f67acb42
Ismail <ghvh8176@gmail.com>
2025-11-04 12:11:51 -0600

    prints

--- Full-stack Django/insight.py
+++ Full-stack Django/insight.py
@@ -1,4 +1,15 @@
 '''
 1) declaring variable: name = 'John Doe'
+2) actions of print(): print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+print('Footballers:', end=' ')
+print('Ronaldo', end='.') # Output: Footballers: Ronaldo.
+
+with open('output.txt', 'w') as f:
+  print('Hello world!', file=f) #will print into file: output.txt
+  
+import time
+print('Processing...', end=' ', flush=True)
+time.sleep(2)
+print('Done!')
 
 '''
