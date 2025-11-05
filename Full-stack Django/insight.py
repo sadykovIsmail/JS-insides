84e2cc8b9c937fe001983308ee176398f8913857
Ismail <ghvh8176@gmail.com>
2025-11-05 14:21:07 -0600

    n

--- Full-stack Django/insight.py
+++ Full-stack Django/insight.py
@@ -26,7 +26,7 @@ print('Hello' in my_str)  # True
 6) actions for strings: upper() #uppercase , lower(), strip() #removes space bef or aft  , my_str.replace('hello', 'hi')  , split() splits to the list,,
 join(list),  .startswith('hello') #checks  , .endswith('world') #checks , .find('world') # 6 , .count('o') # 2  , .capitalize() #capitalizes  ,  .isupper() # checks 
 .islower() # checks  ,  .title('hello world') # Hello World  , bool() #checks truthy or falsly
-str.maketrans(from, to) #builds mapping  ,  string.translate(mapping) applies that translate
+str.maketrans(from, to) #builds mapping  ,  string.translate(mapping) applies that translate  , str in text # checks for existance
 
 7) Integers and Floats: print(type(my_int_1)) # <class 'int'>  ,  my_float_2 % my_float_1 #will return the remainder of division
 print(5.8 // 2)  # 5.8 ÷ 2 = 2.9  ,  pow(2, 3)  # same as 2 ** 3 → 8  ,  int() , float()  , round(my_int_2, 1) # rounds to 1 decimal places  , abs(-15) #15 
