e0bfaa4f88e795fb0258c0f91d8630fcdbb7cdd4
Ismail <ghvh8176@gmail.com>
2025-12-14 20:47:44 -0600

    inheritance

--- Full-stack Django/insight.py
+++ Full-stack Django/insight.py
@@ -131,5 +131,28 @@ delattr(obj, name)	        Delete attribute	             delattr(p, "age")
 24) import datetime 
 now = datetime.datetime.now()
 print(now.strftime("%Y-%m-%d-%H-%M-%S"))  # Output: 2024-03-15 (year-month-day-hour-minute-second with - separator)
-zzzzzzzzzzzzzz
+
+25) Inheritance:
+class Walker:
+    def walk(self):
+        return 'I can walk on land'
+
+class Swimmer:
+    def swim(self):
+        return 'I can swim in water'
+
+# Amphibian inherits from both Walker and Swimmer
+class Amphibian(Walker, Swimmer):
+    def __init__(self, name):
+        self.name = name
+
+    def introduce(self):
+        return f"I'm {self.name} the frog. {self.walk()} and {self.swim()}."
+
+frog = Amphibian('Freddy')
+print(frog.introduce())
+# Output: I'm Freddy the frog. I can walk on land and I can swim in water.
+
+base = super().sound() means:
+#Call the parent class’s sound() method and save its result.
 '''
