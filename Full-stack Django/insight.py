3aec41b37984043af403b7944a5a925895509d16
Ismail <ghvh8176@gmail.com>
2025-12-20 09:46:59 -0600

    d

--- Full-stack Django/insight.py
+++ Full-stack Django/insight.py
@@ -266,4 +266,30 @@ def binary_search(arr, target):
             high = mid - 1
 
     return -1
+    
+32) Merge sort
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    sorted_list = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
+
+    sorted_list.extend(left[i:])
+    sorted_list.extend(right[j:])
+
+    return sorted_list
 '''
