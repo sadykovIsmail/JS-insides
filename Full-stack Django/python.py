e2d83a1a115f34ca2893cd6fcf642b1e9b052be6
Ismail <ghvh8176@gmail.com>
2026-02-09 15:01:16 -0600

    s

--- Full-stack Django/python.py
+++ Full-stack Django/python.py
@@ -6,32 +6,32 @@ print('Ronaldo', end='.') # Output: Footballers: Ronaldo.
 
 with open('output.txt', 'w') as f:
   print('Hello world!', file=f) #will print into file: output.txt
-  
+
 import time
 print('Processing...', end=' ', flush=True)
 time.sleep(2)
 print('Done!')
 
-3) data types: integer: 4, -5 , float:4.50 , complex: 3 + 4j , set:{4, 2, 0}, dictionary: {'name': 'Ismail'} , typle: (4,2,0) , range(5) #(0, 5) , 
+3) data types: integer: 4, -5 , float:4.50 , complex: 3 + 4j , set:{4, 2, 0}, dictionary: {'name': 'Ismail'} , typle: (4,2,0) , range(5) #(0, 5) ,
 list = [22, 'Hello', 3.24, true] , None # is nothing, to check: type() , checking: isinstance(True, bool) # True
 
 4) immutable data types: string, integer, float, boolean, tuple, and range. #can reassing but not change the value
 
 5) Strings: multiline strings: my_str_3 = """Multiline
-string"""   using "" : quote = 'She said, "Hello World!"' or "She said, \"Hello!\""  , 
-str + int = typeError  ,  print(f'The sum of {num1} and {num2} is {num1 + num2}') # The sum of 5 and 10 is 15  , len() #checks the length 
+string"""   using "" : quote = 'She said, "Hello World!"' or "She said, \"Hello!\""  ,
+str + int = typeError  ,  print(f'The sum of {num1} and {num2} is {num1 + num2}') # The sum of 5 and 10 is 15  , len() #checks the length
 hello world  print(my_str[-2]) # l  , sentence[start:stop] # start included stop excluded  , print(my_str[8:])  # rld  , print(my_str[0:11:2])  # Hlowrd   , print(my_str[::-1]) # dlrow olleH
 print('Hello' in my_str)  # True
 
 6) actions for strings: .upper() #uppercase , .lower(), .strip() #removes space bef or aft  , my_str.replace('hello', 'hi')  , split() splits to the list by space,,
-join(list),  .startswith('hello') #checks  , .endswith('world') #checks , .find('world') # 6 , .count('o') # 2  , .capitalize() #capitalizes  ,  .isupper() # checks 
+join(list),  .startswith('hello') #checks  , .endswith('world') #checks , .find('world') # 6 , .count('o') # 2  , .capitalize() #capitalizes  ,  .isupper() # checks
 .islower() # checks  ,  .title('hello world') # Hello World  , bool() #checks truthy or falsly
 str.maketrans(from, to) #builds mapping  ,  string.translate(mapping) applies that translate  , str in text # checks for existance  , list(str) #splits by characters
 
 7) Integers and Floats: print(type(my_int_1)) # <class 'int'>  ,  my_float_2 % my_float_1 #will return the remainder of division
-print(5.8 // 2)  # 5.8 ÷ 2 = 2.9  ,  pow(2, 3)  # same as 2 ** 3 → 8  ,  int() , float()  , round(my_int_2, 1) # rounds to 1 decimal places  , abs(-15) #15 
+print(5.8 // 2)  # 2  ,  pow(2, 3)  # same as 2 ** 3 → 8  ,  int() , float()  , round(my_int_2, 1) # rounds to 1 decimal places  , abs(-15) #15
  bin(my_int) # base of 2  , oct(int) # base of 8 , hex(int) # base of 16 ,  a ^ a = 0  , list(str(int)) # to split
- 
+
 8) Scope: LEGB  , nonlocal res  # Allow modification of an enclosing variable
 global my_var_2  # Makes it globally usable or can change the global var inside function
 
@@ -40,25 +40,25 @@ global my_var_2  # Makes it globally usable or can change the global var inside 
 10) "and",  "or",  "not" operators   ,  range(start, stop, step)
 
 11)del developer[1] #deletes the item index 1 from developer
-        
+
 12)Lists: developer = ['Alice', 34, 'Rust Developer'] unpacking name, age, job = developer
 developer[2][1] # inside the nested list  ,  name, *rest = developer # rest == rest of after 1st el.
 append() # adds element to last   ,   extend() # adds elements from list to list    ,   numbers.insert(2, 2.5)
-.remove(el) # removes el    ,   .pop(index) # removes at index and returns value if not index return last and removes   ,    clear() #clears   ,   sort() #in place sort 
+.remove(el) # removes el    ,   .pop(index) # removes at index and returns value if not index return last and removes   ,    clear() #clears   ,   sort() #in place sort
 .sorted() #returns sorted list   ,   .reverse() # in place reverse   ,  index(item) # founds the items first index  , copy() #copies list,  list(set(lst)) # to del duplicate
 next((k for k, v in enumerate(example) if v == 'b')) # to find by value
 
-13) Tuple: unpacking, slice, same list  ,  .count(el) #counts el  ,  index(el , start, end) # index of el  ,  
+13) Tuple: unpacking, slice, same list  ,  .count(el) #counts el  ,  index(el , start, end) # index of el  ,
 .sorted(iterable, key=None, reverse=False) # returns new list
 
 14)for language in programming_languages:    # for loop    ,    while guess != secret_number:  # whilie loop
     print(language)   else: , break, continue  # works for both
-    
-15)Iterable Operations: list(enumerate(languages, start))   ,    list(zip(developers, ids))  #combines together   ,   
+
+15)Iterable Operations: list(enumerate(languages, start))   ,    list(zip(developers, ids))  #combines together   ,
 for name, id in zip(developers, ids):
-    print(f'Name: {name}')
+    print(f'Name: {namef
     print(f'ID: {id}')
-    
+
 even_numbers = [num for num in range(21) if num % 2 == 0]  # without else
 result = ['Even' if num % 2 == 0 else 'Odd' for num in numbers]  # with even included
 filter(function, iterable)   # filters   ,    map(function, iterable)  # changes each    ,   sum(iterable, start=0) #sums
@@ -69,7 +69,7 @@ dictionary[key] = new value    ,   dictionary.get(key, default)   ,  dic.keys() 
 pizza.update({ 'price': 15, 'total_time': 25 })  , student.pop('key', 'default') #gives value removes key  ,  .popitem() #removes and gives the last key and value like tuple
 for key, value in dictionary.items():      ,     for index, product in enumerate(products.items(), 1):
     dictionary[key] = round(value * 0.8)   ,         print(index, product)   ,  ** from dic to function parameters print(sum(**nums)) # 7  ,  key = (k for k, v in data.items() if v == value) #to find key with value
-    
+
 17)set()              # creates an empty set
 {}                 # creates an empty dictionary (NOT a set)
 
@@ -150,7 +150,7 @@ setattr(obj, name, value)	Set attribute value	             setattr(p, "age", 30)
 hasattr(obj, name)	        Check if attribute exists	     hasattr(p, "age")
 delattr(obj, name)	        Delete attribute	             delattr(p, "age")
 
-24) import datetime 
+24) import datetime
 now = datetime.datetime.now()
 print(now.strftime("%Y-%m-%d-%H-%M-%S"))  # Output: 2024-03-15 (year-month-day-hour-minute-second with - separator)
 
@@ -241,12 +241,12 @@ for animal in animals:
 class Animal(ABC):
     @abstractmethod # makes method required to all subclasses
     def make_sound(self):
-        pass 
-        
-        
+        pass
+
+
 29) import random
  return random.choice([])
- 
+
 30) Data structures:
 Stacks: like dish last removes first, Push: adding, POP: removing, peek: return the last element
 Queues: like queues first come first leave , Enqueue: adding to back , Deque: removing first
@@ -257,7 +257,7 @@ def binary_search(arr, target):
     high = len(arr) - 1
 
     while low <= high:
-        mid = (low + high) // 2  
+        mid = (low + high) // 2
 
         if arr[mid] == target:
             return mid
@@ -267,7 +267,7 @@ def binary_search(arr, target):
             high = mid - 1
 
     return -1
-    
+
 32) Merge sort
 def merge_sort(arr):
     if len(arr) <= 1:
@@ -295,7 +295,7 @@ def merge_sort(arr):
     return sorted_list
 
 
-33) Mixed operations 
+33) Mixed operations
 # Min, Max, Sum, Len
 # -------------------------------
 print(min(numbers))           # 2
